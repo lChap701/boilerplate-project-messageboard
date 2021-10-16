aebc794bd372f1a121e2bbde3aba05a2954cc920
lChap701 <chapmanlucas201@gmail.com>
2021-10-16 11:11:39 -0500

    Formatted code and added JSDoc comments

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,14 @@
-'use strict';
+"use strict";
 
+/**
+ * Module that handles most of the routing
+ * @module ./routes/api
+ *
+ * @param {*} app   Represents the Express application
+ *
+ */
 module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
-    
-  app.route('/api/replies/:board');
+  app.route("/api/threads/:board");
 
+  app.route("/api/replies/:board");
 };
