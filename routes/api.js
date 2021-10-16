cb0e445782c37354a946ea60c9f96f00a584b913
lChap701 <chapmanlucas201@gmail.com>
2021-10-16 13:39:09 -0500

    Started routing

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,5 @@
 "use strict";
+const ThreadController = require("../controllers/threads");
 
 /**
  * Module that handles most of the routing
@@ -8,7 +9,17 @@
  *
  */
 module.exports = function (app) {
-  app.route("/api/threads/:board");
+  app
+    .route("/api/threads/:board")
+    .get((req, res) => {
+      res.json(ThreadController.getThreads(req.params.board));
+    })
+
+    .post((req, res) => {})
+
+    .put((req, res) => {})
+
+    .delete((req, res) => {});
 
   app.route("/api/replies/:board");
 };
