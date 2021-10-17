0795b6f285fe06eced1d5f01592dac39b11ca1c6
lChap701 <chapmanlucas201@gmail.com>
2021-10-17 12:55:09 -0500

    Implemented board controller

--- controllers/threads.js
+++ controllers/threads.js
@@ -1,4 +1,5 @@
 const crud = require("../crud");
+const BoardController = require("../controllers/boards");
 
 /**
  * Module that handles all HTTP requests for threads
@@ -9,20 +10,22 @@ module.exports = class ThreadController {
   /**
    * Gets all threads for GET requests
    * @param {String} board    Represents the board to search for
-   * @returns                 Returns all threads that were found
+   * @returns                 Returns all threads that were found or an error message
    */
   static getThreads(board) {
     let results = [];
 
-    crud.getBoard(board).then((boardObj) => {
-      if (boardObj == null) return;
-      crud
-        .getThreads(boardObj.name)
-        .then(
-          (threads) =>
-            (results = threads.sort((a, b) => b.bumped_on - a.bumped_on))
-        );
-    });
+    if (!BoardController.boardExists(board)) {
+      let result = BoardController.saveBoard(board);
+      if (typeof result != "object") return result;
+    }
+
+    crud
+      .getThreads(BoardController.findBoard(board).name)
+      .then(
+        (threads) =>
+          (results = threads.sort((a, b) => b.bumped_on - a.bumped_on))
+      );
 
     return results.length > 10 ? results.slice(0, 10) : results;
   }
