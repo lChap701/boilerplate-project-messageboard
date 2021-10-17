42e2664f19258f613b384c0d7f86cd4a987e549a
lChap701 <chapmanlucas201@gmail.com>
2021-10-17 15:09:52 -0500

    Finished routing for threads

--- routes/api.js
+++ routes/api.js
@@ -35,7 +35,14 @@ module.exports = function (app) {
       res.send(ThreadController.putThread(req.params.board, id));
     })
 
-    .delete((req, res) => {});
+    .delete((req, res) =>
+      res.send(
+        ThreadController.deleteThread(req.params.board, {
+          thread_id: req.body.thread_id,
+          delete_password: req.body.delete_password,
+        })
+      )
+    );
 
   app.route("/api/replies/:board");
 };
