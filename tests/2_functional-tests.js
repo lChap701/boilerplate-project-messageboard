3828b489f3481294a7ed7e861a8e4e1bd99039af
lChap701 <chapmanlucas201@gmail.com>
2021-10-20 13:01:31 -0500

    Removed redundant module

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,7 +1,6 @@
 const chaiHttp = require("chai-http");
 const chai = require("chai");
 const assert = chai.assert;
-const bcrypt = require("bcrypt");
 const server = require("../server");
 const crud = require("../crud");
 
